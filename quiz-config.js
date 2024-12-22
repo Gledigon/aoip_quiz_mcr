a2228247cbcae57a01bc9f2904b07b05e59b662b
Gledigon <149788710+Gledigon@users.noreply.github.com>
2024-12-22 20:15:42 +0100

    Update quiz-config.js

--- quiz-config.js
+++ quiz-config.js
@@ -11,7 +11,6 @@ const questions = [
     {
         id: 1,
         text: "Toby er onsite i oppgjøret mellom Gneist - Jubb FK. Han har med seg en via Tieline, men mener noen porter må være stengt i brannmuren for å motta lyd fra en Tieline Via",
-        text: "Dere ringer nettverk og ønsker å få klarhet i dette. Hva spør dere om? ",
         allowImage: true
     },
     {
